eb1e4cd4d2399a249dabe32c2c669b27dc5da3b7
orange <orange@jiangyu>
2014-03-19 17:54:40 +0800

    c

---
+++ graduate3/src/main/java/graduation/webspring/SystemController.java
@@ -0,0 +1,18 @@
+package graduation.webspring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author jiangyukun
+ * @since 2014-03-19 17:53
+ */
+@Controller
+@RequestMapping("/admin")
+public class SystemController {
+
+	@RequestMapping({ "/", "index" })
+	public String index() {
+		return "admin/index";
+	}
+}
